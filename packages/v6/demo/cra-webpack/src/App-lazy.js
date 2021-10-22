2612654e53b8c2c5bfec341ccc183276da1d991a
diy <dengry.diy@gmail.com>
2021-10-22 10:34:56 +0800

    feat: react-route v6 conventional route design

--- packages/v6/demo/cra-webpack/src/App-lazy.js
+++ packages/v6/demo/cra-webpack/src/App-lazy.js
@@ -1,29 +1,20 @@
-import  { Suspense, lazy } from 'react';
+import  { createElement, Suspense, lazy } from 'react';
 import routeGenerate from 'react-conventional-route-v6'
 import { useRoutes } from 'react-router-dom';
-
-// step0: define a component for choose the Type at Runtime
-function RuntimeRoute ({componet}) {
-  const RoutComp = lazy(() => componet)
-  return <RoutComp />
-}
 
 // step1: get all routing components
 const routeCtx = require.context('./pages', true, /((?<!__tests__).)*\.route\.js/, 'lazy-once')
 
 // step2: generate all routes
-const routes = routeGenerate(routeCtx.keys(), (f)=> <RuntimeRoute componet={routeCtx(f)} />, '.')
+const routes = routeGenerate(routeCtx.keys(), (f)=> createElement(lazy(() => routeCtx(f))), '.')
 // you can see all routes array
 console.log(routes)
 
-// step3: return useRoutes(routes)
-function RoutesEle () {
-  return useRoutes(routes)
-}
+// step3: useRoutes(routes)
 function App() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <RoutesEle />
+      {useRoutes(routes)}
     </Suspense>
   )
 }
--- packages/v6/demo/cra-webpack/src/App-sync.js
+++ packages/v6/demo/cra-webpack/src/App-sync.js
@@ -1,23 +1,22 @@
+import  { createElement, Fragment } from 'react';
 import routeGenerate from 'react-conventional-route-v6'
 import { useRoutes } from 'react-router-dom';
-
-// step0: define a component for choose the Type at Runtime
-function RuntimeRoute ({componet}) {
-  const RoutComp = componet
-  return <RoutComp />
-}
 
 // step1: get all routing components
 const routeCtx = require.context('./pages', true, /((?<!__tests__).)*\.route\.js/)
 
 // step2: generate all routes, don't forget route.__esModule ? route.default : route
-const routes = routeGenerate(routeCtx.keys(), (f)=> <RuntimeRoute componet={routeCtx(f).default} />, '.')
+const routes = routeGenerate(routeCtx.keys(), (f)=> createElement(routeCtx(f).default), '.')
 // you can see all routes array
 console.log(routes)
 
-// step3: return useRoutes(routes)
+// step3: useRoutes(routes)
 function App() {
-  return useRoutes(routes)
+  return (
+    <Fragment>
+      {useRoutes(routes)}
+    </Fragment>
+  )
 }
 
 export default App;
